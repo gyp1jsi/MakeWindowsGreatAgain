70b439c26d92045d2cf30afaa597336544537ea4
gyp1jsi <114802285+gyp1jsi@users.noreply.github.com>
2022-10-01 11:35:39 +0200

    Update main.ps1
    Adding some apps to the uninstall list (Messenger, Tik Tok, Instagram, Spotify)

--- MakeWindowsGreatAgain 0.1.0/main.ps1
+++ MakeWindowsGreatAgain 0.1.0/main.ps1
@@ -100,6 +100,9 @@ $AppXApps = @(
         "*WinZipUniversal*"
         "*Wunderlist*"
         "*XING*"
+        "*Messenger*"
+        "*Instagram*"
+        "*TikTok*"
 
         # Apps which other apps depend on
         "Microsoft.Advertising.Xaml"
@@ -140,6 +143,7 @@ $AppXApps = @(
 
         "*Netflix*"                        # Netflix
         "*SpotifyMusic*"                   # Spotify
+        "*Spotify*"                        #seems SpotifyMusic does not actually remove Spotify
 
         # [DIY] Can't be reinstalled
 
