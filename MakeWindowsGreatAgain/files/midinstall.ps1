5093c2298282fe5d4072dad10ae293da0e350ac8
gyp1jsi <gianlucajamesbufano@gmail.com>
2023-04-16 18:03:24 +0200

    modified:   MakeWindowsGreatAgain/Safe-Mode-Scripts/safemodescripts.ps1 new file:   MakeWindowsGreatAgain/files/hard.ps1 moved:    MakeWindowsGreatAgain/include/lib/download-web-file.psm1 -> MakeWindowsGreatAgain/files/include/lib/download-web-file.psm1 moved:    MakeWindowsGreatAgain/include/lib/get-hardware-info.psm1 -> MakeWindowsGreatAgain/files/include/lib/get-hardware-info.psm1 moved:    MakeWindowsGreatAgain/include/lib/install-font.psm1 -> MakeWindowsGreatAgain/files/include/lib/install-font.psm1 moved:    MakeWindowsGreatAgain/include/lib/manage-software.psm1 -> MakeWindowsGreatAgain/files/include/lib/manage-software.psm1 moved:    MakeWindowsGreatAgain/include/lib/new-shortcut.psm1 -> MakeWindowsGreatAgain/files/include/lib/new-shortcut.psm1 moved:    MakeWindowsGreatAgain/include/lib/open-file.psm1 -> MakeWindowsGreatAgain/files/include/lib/open-file.psm1 moved:    MakeWindowsGreatAgain/include/lib/original/New-FolderForced.psm1 -> MakeWindowsGreatAgain/files/include/lib/original/New-FolderForced.psm1 moved:    MakeWindowsGreatAgain/include/lib/remove-uwp-appx.psm1 -> MakeWindowsGreatAgain/files/include/lib/remove-uwp-appx.psm1 moved:    MakeWindowsGreatAgain/include/lib/select-folder-gui.psm1 -> MakeWindowsGreatAgain/files/include/lib/select-folder-gui.psm1 moved:    MakeWindowsGreatAgain/include/lib/set-console-style.psm1 -> MakeWindowsGreatAgain/files/include/lib/set-console-style.psm1 moved:    MakeWindowsGreatAgain/include/lib/set-scheduled-task-state.psm1 -> MakeWindowsGreatAgain/files/include/lib/set-scheduled-task-state.psm1 moved:    MakeWindowsGreatAgain/include/lib/set-service-startup.psm1 -> MakeWindowsGreatAgain/files/include/lib/set-service-startup.psm1 moved:    MakeWindowsGreatAgain/include/lib/set-windows-feature-state.psm1 -> MakeWindowsGreatAgain/files/include/lib/set-windows-feature-state.psm1 moved:    MakeWindowsGreatAgain/include/lib/show-dialog-window.psm1 -> MakeWindowsGreatAgain/files/include/lib/show-dialog-window.psm1 moved:    MakeWindowsGreatAgain/include/lib/start-logging.psm1 -> MakeWindowsGreatAgain/files/include/lib/start-logging.psm1 moved:    MakeWindowsGreatAgain/include/lib/title-templates.psm1 -> MakeWindowsGreatAgain/files/include/lib/title-templates.psm1 moved:    MakeWindowsGreatAgain/include/lib/ui-helper.psm1 -> MakeWindowsGreatAgain/files/include/lib/ui-helper.psm1 moved:    MakeWindowsGreatAgain/include/lib/unregister-duplicated-power-plans.psm1 -> MakeWindowsGreatAgain/files/include/lib/unregister-duplicated-power-plans.psm1 moved:    MakeWindowsGreatAgain/include/tmp/ShutUp10/ooshutup10.cfg -> MakeWindowsGreatAgain/files/include/tmp/ShutUp10/ooshutup10.cfg moved:    MakeWindowsGreatAgain/include/tmp/Temporary files will download here.txt -> MakeWindowsGreatAgain/files/include/tmp/Temporary files will download here.txt moved:    MakeWindowsGreatAgain/include/utils/individual-tweaks.psm1 -> MakeWindowsGreatAgain/files/include/utils/individual-tweaks.psm1 moved:    MakeWindowsGreatAgain/include/utils/install-individual-system-apps.psm1 -> MakeWindowsGreatAgain/files/include/utils/install-individual-system-apps.psm1 new file:   MakeWindowsGreatAgain/files/midinstall.ps1 new file:   MakeWindowsGreatAgain/files/soft.ps1 modified:   MakeWindowsGreatAgain/main.ps1

---
+++ MakeWindowsGreatAgain/files/midinstall.ps1
@@ -0,0 +1,230 @@
+# For who has already installed Windows and configured everything, maybe after an update.
+Write-Output "Do you want to disable and stop unuseful services? (y/n)"
+$confirm = Read-Host
+if ($confirm -eq "y") {
+    Write-Output "The unuseful services will be removed."
+    Import-Module -DisableNameChecking $PSScriptRoot\include\lib\"get-hardware-info.psm1"
+    Import-Module -DisableNameChecking $PSScriptRoot\include\lib\"set-service-startup.psm1"
+    Import-Module -DisableNameChecking $PSScriptRoot\include\lib\"title-templates.psm1"
+
+# Adapted from: https://youtu.be/qWESrvP_uU8
+# Adapted from: https://github.com/ChrisTitusTech/win10script
+# Adapted from: https://gist.github.com/matthewjberger/2f4295887d6cb5738fa34e597f457b7f
+# Adapted from: https://github.com/Sycnex/Windows10Debloater
+
+function Optimize-ServicesRunning() {
+    [CmdletBinding()]
+    param (
+        [Switch] $Revert
+    )
+
+    $IsSystemDriveSSD = $(Get-OSDriveType) -eq "SSD"
+    $EnableServicesOnSSD = @("SysMain")
+
+    # Services which will be totally disabled
+    $ServicesToDisabled = @(
+        "DiagTrack"                                 # DEFAULT: Automatic | Connected User Experiences and Telemetry
+        "diagnosticshub.standardcollector.service"  # DEFAULT: Manual    | Microsoft (R) Diagnostics Hub Standard Collector Service
+        "dmwappushservice"                          # DEFAULT: Manual    | Device Management Wireless Application Protocol (WAP)
+        "Fax"                                       # DEFAULT: Manual    | Fax Service
+        "fhsvc"                                     # DEFAULT: Manual    | Fax History Service
+        "GraphicsPerfSvc"                           # DEFAULT: Manual    | Graphics performance monitor service
+        "HomeGroupListener"                         # NOT FOUND (Win 10+)| HomeGroup Listener
+        "HomeGroupProvider"                         # NOT FOUND (Win 10+)| HomeGroup Provider
+        "lfsvc"                                     # DEFAULT: Manual    | Geolocation Service
+        "MapsBroker"                                # DEFAULT: Automatic | Downloaded Maps Manager
+        "PcaSvc"                                    # DEFAULT: Automatic | Program Compatibility Assistant (PCA)
+        "RemoteAccess"                              # DEFAULT: Disabled  | Routing and Remote Access
+        "RemoteRegistry"                            # DEFAULT: Disabled  | Remote Registry
+        "RetailDemo"                                # DEFAULT: Manual    | The Retail Demo Service controls device activity while the device is in retail demo mode.
+        "SysMain"                                   # DEFAULT: Automatic | SysMain / Superfetch (100% Disk usage on HDDs)
+        "TrkWks"                                    # DEFAULT: Automatic | Distributed Link Tracking Client
+        "WSearch"                                   # DEFAULT: Automatic | Windows Search (100% Disk usage on HDDs, dangerous on SSDs too)
+        # - Services which cannot be disabled (and shouldn't)
+        #"wscsvc"                                   # DEFAULT: Automatic | Windows Security Center Service
+        #"WdNisSvc"                                 # DEFAULT: Manual    | Windows Defender Network Inspection Service
+    )
+
+    # Making the services to run only when needed as 'Manual' | Remove the # to set to Manual
+    $ServicesToManual = @(
+        "BITS"                           # DEFAULT: Manual    | Background Intelligent Transfer Service
+        "edgeupdate"                     # DEFAULT: Automatic | Microsoft Edge Update Service
+        "edgeupdatem"                    # DEFAULT: Manual    | Microsoft Edge Update Service²
+        "FontCache"                      # DEFAULT: Automatic | Windows Font Cache
+        "iphlpsvc"                       # DEFAULT: Automatic | IP Helper Service (IPv6 (6to4, ISATAP, Port Proxy and Teredo) and IP-HTTPS)
+        "lmhosts"                        # DEFAULT: Manual    | TCP/IP NetBIOS Helper
+        "ndu"                            # DEFAULT: Automatic | Windows Network Data Usage Monitoring Driver (Shows network usage per-process on Task Manager)
+        "wuauserv"                       # DEFAULT: Automatic | Windows Update
+        "UsoSvc"                         # DEFAULT: Automatic | Update Orchestrator Service (Manages the download and installation of Windows updates)
+        #"NetTcpPortSharing"             # DEFAULT: Disabled  | Net.Tcp Port Sharing Service
+        "PhoneSvc"                       # DEFAULT: Manual    | Phone Service (Manages the telephony state on the device)
+        "SCardSvr"                       # DEFAULT: Manual    | Smart Card Service
+        "SharedAccess"                   # DEFAULT: Manual    | Internet Connection Sharing (ICS)
+        "stisvc"                         # DEFAULT: Automatic | Windows Image Acquisition (WIA) Service
+        "WbioSrvc"                       # DEFAULT: Manual    | Windows Biometric Service (required for Fingerprint reader / Facial detection)
+        "Wecsvc"                         # DEFAULT: Manual    | Windows Event Collector Service
+        "WerSvc"                         # DEFAULT: Manual    | Windows Error Reporting Service
+        "wisvc"                          # DEFAULT: Manual    | Windows Insider Program Service
+        "WMPNetworkSvc"                  # DEFAULT: Manual    | Windows Media Player Network Sharing Service
+        "WpnService"                     # DEFAULT: Automatic | Windows Push Notification Services (WNS)
+        # - Diagnostic Services
+        "DPS"                            # DEFAULT: Automatic | Diagnostic Policy Service
+        "WdiServiceHost"                 # DEFAULT: Manual    | Diagnostic Service Host
+        "WdiSystemHost"                  # DEFAULT: Manual    | Diagnostic System Host
+        # - Bluetooth services
+        "BTAGService"                    # DEFAULT: Manual    | Bluetooth Audio Gateway Service
+        "BthAvctpSvc"                    # DEFAULT: Manual    | AVCTP Service
+        "bthserv"                        # DEFAULT: Manual    | Bluetooth Support Service
+        "RtkBtManServ"                   # DEFAULT: Automatic | Realtek Bluetooth Device Manager Service
+        # - Xbox services
+        "XblAuthManager"                 # DEFAULT: Manual    | Xbox Live Auth Manager
+        "XblGameSave"                    # DEFAULT: Manual    | Xbox Live Game Save
+        "XboxGipSvc"                     # DEFAULT: Manual    | Xbox Accessory Management Service
+        "XboxNetApiSvc"                  # DEFAULT: Manual    | Xbox Live Networking Service
+        # - NVIDIA services
+        "NVDisplay.ContainerLocalSystem" # DEFAULT: Automatic | NVIDIA Display Container LS (NVIDIA Control Panel)
+        "NvContainerLocalSystem"         # DEFAULT: Automatic | NVIDIA LocalSystem Container (GeForce Experience / NVIDIA Telemetry)
+        # - Printer services
+        #"PrintNotify"                   # DEFAULT: Manual    | WARNING! REMOVING WILL TURN PRINTING LESS MANAGEABLE | Printer Extensions and Notifications
+        #"Spooler"                       # DEFAULT: Automatic | WARNING! REMOVING WILL DISABLE PRINTING              | Print Spooler
+        # - Wi-Fi services
+        #"WlanSvc"                       # DEFAULT: Manual (No Wi-Fi devices) / Automatic (Wi-Fi devices) | WARNING! REMOVING WILL DISABLE WI-FI, DON'T TELL ME I DIDN'T WARN YOU, LITTLE PP BITCHES | WLAN AutoConfig
+        # - 3rd Party Services
+        "gupdate"                        # DEFAULT: Automatic | Google Update Service
+        "gupdatem"                       # DEFAULT: Manual    | Google Update Service²
+    )
+
+    Write-Title -Text "Services tweaks"
+    Write-Section -Text "Disabling services from Windows"
+
+    If ($Revert) {
+        Write-Status -Types "*", "Service" -Status "Reverting the tweaks is set to '$Revert'." -Warning
+        $CustomMessage = { "Resetting $Service ($((Get-Service $Service).DisplayName)) as 'Manual' on Startup ..." }
+        Set-ServiceStartup -Manual -Services $ServicesToDisabled -Filter $EnableServicesOnSSD -CustomMessage $CustomMessage
+    } Else {
+        Set-ServiceStartup -Disabled -Services $ServicesToDisabled -Filter $EnableServicesOnSSD
+    }
+
+    Write-Section -Text "Enabling services from Windows"
+
+    If ($IsSystemDriveSSD -or $Revert) {
+        $CustomMessage = { "The $Service ($((Get-Service $Service).DisplayName)) service works better in 'Automatic' mode on SSDs ..." }
+        Set-ServiceStartup -Automatic -Services $EnableServicesOnSSD -CustomMessage $CustomMessage
+    }
+
+    Set-ServiceStartup -Manual -Services $ServicesToManual
+}
+
+function Main() {
+    # List all services:
+    #Get-Service | Select-Object StartType, Status, Name, DisplayName, ServiceType | Sort-Object StartType, Status, Name | Out-GridView
+
+    If (!$Revert) {
+        Optimize-ServicesRunning # Enable essential Services and Disable bloating Services
+    } Else {
+        Optimize-ServicesRunning -Revert
+    }
+}
+
+Main
+}
+else {
+    Write-Output "Unuseful services will not be disabled."
+}
+
+Write-Output "Do you want to optimize Task Scheduler tasks? (y/n)"
+$confirm = Read-Host
+if ($confirm -eq "y") {
+    #Optimizes Task Scheduler tasks
+Import-Module -DisableNameChecking $PSScriptRoot\include\lib\"set-scheduled-task-state.psm1"
+Import-Module -DisableNameChecking $PSScriptRoot\include\lib\"title-templates.psm1"
+
+# Adapted from: https://youtu.be/qWESrvP_uU8
+# Adapted from: https://github.com/ChrisTitusTech/win10script
+# Adapted from: https://gist.github.com/matthewjberger/2f4295887d6cb5738fa34e597f457b7f
+# Adapted from: https://github.com/Sycnex/Windows10Debloater
+# Adapted from: https://github.com/kalaspuffar/windows-debloat
+
+function Optimize-TaskScheduler() {
+    [CmdletBinding()]
+    param (
+        [Switch] $Revert
+    )
+
+    # Adapted from: https://docs.microsoft.com/en-us/windows-server/remote/remote-desktop-services/rds-vdi-recommendations#task-scheduler
+    $DisableScheduledTasks = @(
+        "\Microsoft\Office\OfficeTelemetryAgentLogOn"
+        "\Microsoft\Office\OfficeTelemetryAgentFallBack"
+        "\Microsoft\Office\Office 15 Subscription Heartbeat"
+        "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+        "\Microsoft\Windows\Application Experience\ProgramDataUpdater"
+        "\Microsoft\Windows\Application Experience\StartupAppTask"
+        "\Microsoft\Windows\Autochk\Proxy"
+        "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"         # Recommended state for VDI use
+        "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask"       # Recommended state for VDI use
+        "\Microsoft\Windows\Customer Experience Improvement Program\Uploader"
+        "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip"              # Recommended state for VDI use
+        "\Microsoft\Windows\Defrag\ScheduledDefrag"                                       # Recommended state for VDI use
+        "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector"
+        "\Microsoft\Windows\Location\Notifications"                                       # Recommended state for VDI use
+        "\Microsoft\Windows\Location\WindowsActionDialog"                                 # Recommended state for VDI use
+        "\Microsoft\Windows\Maps\MapsToastTask"                                           # Recommended state for VDI use
+        "\Microsoft\Windows\Maps\MapsUpdateTask"                                          # Recommended state for VDI use
+        "\Microsoft\Windows\Mobile Broadband Accounts\MNO Metadata Parser"                # Recommended state for VDI use
+        "\Microsoft\Windows\Power Efficiency Diagnostics\AnalyzeSystem"                   # Recommended state for VDI use
+        "\Microsoft\Windows\Retail Demo\CleanupOfflineContent"                            # Recommended state for VDI use
+        "\Microsoft\Windows\Shell\FamilySafetyMonitor"                                    # Recommended state for VDI use
+        "\Microsoft\Windows\Shell\FamilySafetyRefreshTask"                                # Recommended state for VDI use
+        "\Microsoft\Windows\Shell\FamilySafetyUpload"
+        "\Microsoft\Windows\Windows Media Sharing\UpdateLibrary"                          # Recommended state for VDI use
+    )
+
+    $EnableScheduledTasks = @(
+        "\Microsoft\Windows\Maintenance\WinSAT"                     # WinSAT detects incorrect system configurations, that causes performance loss, then sends it via telemetry | Reference (PT-BR): https://youtu.be/wN1I0IPgp6U?t=16
+        "\Microsoft\Windows\RecoveryEnvironment\VerifyWinRE"        # Verify the Recovery Environment integrity, it's the Diagnostic tools and Troubleshooting when your PC isn't healthy on BOOT, need this ON.
+        "\Microsoft\Windows\Windows Error Reporting\QueueReporting" # Windows Error Reporting event, needed to improve compatibility with your hardware
+    )
+
+    Write-Title -Text "Task Scheduler tweaks"
+    Write-Section -Text "Disabling Scheduled Tasks from Windows"
+
+    If ($Revert) {
+        Write-Status -Types "*", "TaskScheduler" -Status "Reverting the tweaks is set to '$Revert'." -Warning
+        $CustomMessage = { "Resetting the $ScheduledTask task as 'Ready' ..." }
+        Set-ScheduledTaskState -Ready -ScheduledTask $DisableScheduledTasks -CustomMessage $CustomMessage
+    } Else {
+        Set-ScheduledTaskState -Disabled -ScheduledTask $DisableScheduledTasks
+    }
+
+    Write-Section -Text "Enabling Scheduled Tasks from Windows"
+    Set-ScheduledTaskState -Ready -ScheduledTask $EnableScheduledTasks
+}
+
+function Main() {
+    # List all Scheduled Tasks:
+    #Get-ScheduledTask | Select-Object -Property State, TaskPath, TaskName, Description | Sort-Object State, TaskPath, TaskName | Out-GridView
+
+    If (!$Revert) {
+        Optimize-TaskScheduler # Disable Scheduled Tasks that causes slowdowns
+    } Else {
+        Optimize-TaskScheduler -Revert
+    }
+}
+
+Main
+}
+else {
+    Write-Output "Task Scheduler tasks will not be optimized."
+}
+
+Write-Output "Do you want to remove Microsoft Edge? (y/n)"
+$confirm = Read-Host
+if ($confirm -eq "y") {
+    Write-Output "Okay. Reboot into safe mode with command prompt, run explorer.exe, and then run the "RUN ME AS ADMIN.bat" file inside the Safe-Mode-Scripts folder."
+}
+else {
+    Write-Output "Edge will not be removed."
+}
+
+Write-Output "End of the script"
+timeout /t 5
